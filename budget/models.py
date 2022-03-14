147a1528e974789d26997c39e60fcbddae72e598
sergey_klimov <s.klimov@bk.ru>
2022-03-14 20:51:00 +0700

    adding abstract models

--- budget/models.py
+++ budget/models.py
@@ -5,6 +5,7 @@ class BankAccount(Model):
     name = models.CharField(
         max_length=200, verbose_name="название", db_index=True, unique=True
     )
+    incoming_balance = models.DecimalField(verbose_name="сумма", decimal_places=2)
 
     class Meta:
         ordering = ("name",)
@@ -15,93 +16,69 @@ class BankAccount(Model):
         return self.name
 
 
-class IncomeGroup(Model):
+class Group(Model):
     name = models.CharField(
         max_length=200, verbose_name="название", db_index=True, unique=True
     )
+
+    class Meta:
+        abstract = True
+
+    def __repr__(self):
+        return self.name
+
+
+class IncomeGroup(Group):
 
     class Meta:
         ordering = ("name",)
         verbose_name = "группа доходов"
         verbose_name_plural = "группы доходов"
 
-    def __repr__(self):
-        return self.name
 
-
-class IncomeCategory(Model):
-    name = models.CharField(
-        max_length=200, verbose_name="название", db_index=True, unique=True
-    )
-    group = models.ForeignKey(
-        IncomeGroup, related_name="categories", on_delete=models.CASCADE
-    )
-
-    class Meta:
-        ordering = ("name",)
-        verbose_name = "категория доходов"
-        verbose_name_plural = "категории доходов"
-
-    def __repr__(self):
-        return self.name
-
-
-class Income(Model):
-    value = models.DecimalField(verbose_name="доход", decimal_places=2)
-    bank_account = models.ForeignKey(
-        BankAccount, related_name="incomes", on_delete=models.CASCADE
-    )
-    category = models.ForeignKey(
-        IncomeCategory, related_name="incomes", on_delete=models.CASCADE
-    )
-    comment = models.CharField(
-        max_length=200, verbose_name="комментарий", null=True, blank=True
-    )
-
-    class Meta:
-        ordering = ("-created_at",)
-        verbose_name = "доход"
-        verbose_name_plural = "доходы"
-
-    def __repr__(self):
-        return "{} {} {}".format(
-            self.__class__.__name__, self.category.name, self.value
-        )
-
-
-class ExpenditureGroup(Model):
-    name = models.CharField(
-        max_length=200, verbose_name="название", db_index=True, unique=True
-    )
+class ExpenditureGroup(Group):
 
     class Meta:
         ordering = ("name",)
         verbose_name = "группа расходов"
         verbose_name_plural = "группы расходов"
 
-    def __repr__(self):
-        return self.name
 
+class Category(Model):
 
-class ExpenditureCategory(Model):
     name = models.CharField(
-        max_length=200, verbose_name="название", db_index=True, unique=True
+        max_length=200, verbose_name="название", db_index=True
     )
     group = models.ForeignKey(
         IncomeGroup, related_name="categories", on_delete=models.CASCADE
     )
 
     class Meta:
+        abstract = True
+        unique_together = ('name', 'group')
+
+    def __repr__(self):
+        return "{}/{}".format(self.group.name, self.name)
+
+
+class IncomeCategory(Category):
+
+    class Meta:
+        ordering = ("name",)
+        verbose_name = "категория доходов"
+        verbose_name_plural = "категории доходов"
+
+
+class ExpenditureCategory(Category):
+
+    class Meta:
         ordering = ("name",)
         verbose_name = "категория расходов"
         verbose_name_plural = "категории расходов"
 
-    def __repr__(self):
-        return self.name
 
-
-class Expenditure(Model):
-    value = models.DecimalField(verbose_name="расход", decimal_places=2)
+class CashFlow(Model):
+    value = models.DecimalField(verbose_name="сумма", decimal_places=2)
     bank_account = models.ForeignKey(
         BankAccount, related_name="expenditures", on_delete=models.CASCADE
     )
@@ -113,11 +90,25 @@ class Expenditure(Model):
     )
 
     class Meta:
-        ordering = ("-created_at",)
-        verbose_name = "расход"
-        verbose_name_plural = "расходы"
+        abstract = True
 
     def __repr__(self):
         return "{} {} {}".format(
             self.__class__.__name__, self.category.name, self.value
         )
+
+
+class Income(CashFlow):
+
+    class Meta:
+        ordering = ("-created_at",)
+        verbose_name = "доход"
+        verbose_name_plural = "доходы"
+
+
+class Expenditure(CashFlow):
+
+    class Meta:
+        ordering = ("-created_at",)
+        verbose_name = "расход"
+        verbose_name_plural = "расходы"

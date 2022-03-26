a314caf769f450e0ec91d9d533531af201b3dde2
sergey_klimov <s.klimov@bk.ru>
2022-03-26 08:57:46 +0700

    Доработаны настройки docker-compose.yml

--- django_budget/settings.py
+++ django_budget/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # Application definition
 
@@ -125,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

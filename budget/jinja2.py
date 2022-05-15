8af6e2e7e1442b7ef8042d8fbcb8999b2bacc618
sergey_klimov <s.klimov@bk.ru>
2022-05-15 16:22:40 +0700

    Подключили jinja2

--- budget/jinja2.py
+++ budget/jinja2.py
@@ -3,11 +3,14 @@ from django.urls import reverse
 
 from jinja2 import Environment
 
+from budget.models import BankAccount
+
 
 def environment(**options):
     env = Environment(**options)
     env.globals.update({
         'static': static,
         'url': reverse,
+        'accounts_list': BankAccount.objects.filter(is_active=True).order_by("-created_at"),
     })
     return env
